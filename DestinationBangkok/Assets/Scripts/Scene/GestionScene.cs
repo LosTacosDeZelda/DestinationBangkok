105b048071ecbb6b056ce40ef6f9663293598ed3
jijimy <jijimy123@gmail.com>
2020-10-14 13:26:51 -0400

    Functionalite des boutons du menu principal

---
+++ DestinationBangkok/Assets/Scripts/Scene/GestionScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+* Script qui gère la gestion de scènes
+* Par : Chen Haoyang
+*/
+
+public class GestionScene : MonoBehaviour
+{
+    public void sceneJeu()
+  {
+    SceneManager.LoadScene("PrototypeNiveau");
+    print("On change la scene");
+  }
+
+    public void quitterJeu()
+  {
+    Application.Quit();
+    print("Le jeu est fermer, tbk.");
+  }
+}
