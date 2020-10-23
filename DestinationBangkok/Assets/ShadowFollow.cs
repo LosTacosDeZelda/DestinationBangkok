1074e0494748c9af1c8cfc799aef2d26c7a99cc0
LosTacosDeZelda <jeudyraphael@gmail.com>
2020-10-23 15:30:32 -0400

    Toon Shading du sac

--- DestinationBangkok/Assets/ShadowFollow.cs
+++ DestinationBangkok/Assets/ShadowFollow.cs
@@ -5,28 +5,11 @@ using UnityEngine;
 public class ShadowFollow : MonoBehaviour
 {
     public GameObject player;
-    SpriteRenderer shadow;
-
-    private void Start()
-    {
-        shadow = gameObject.GetComponent<SpriteRenderer>();
-    }
 
     // Update est appelée des dizaines de fois/seconde
     void Update()
     {
         
-        
-        if (player.GetComponent<PlayerController>().playerFeet.transform == null)
-        {
-            shadow.color = new Color(shadow.color.r, shadow.color.g, shadow.color.b, 0);
-        }
-        else
-        {
-            shadow.color = new Color(shadow.color.r, shadow.color.g, shadow.color.b, 1); 
-
-        }
-
-        transform.position = new Vector3(player.transform.position.x,  player.GetComponent<PlayerController>().playerFeet.point.y + 0.01f, player.transform.position.z);
+        transform.position = new Vector3(player.transform.position.x,  player.GetComponent<PlayerController>().playerFeet.point.y + -0.45f, player.transform.position.z);
     }
 }
