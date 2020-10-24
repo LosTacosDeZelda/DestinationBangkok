77ca4b5416ff9682fba01b5ac52a025ae0e19618
Frank798034 <Frank798034@users.noreply.github.com>
2020-10-24 18:58:22 -0400

    Ajout de la collision avec le pieu et changement de scène
    J'ai ajouté le pieu à une place dans le temple, j'ai ajouté un script au pieu afin qu'il puisse détecter les collisions avec le personnage. Pour l'instant, j'ai ajouté un print pour dire qu'il avait touché le personnage et j'ai mis un changement de scène pour vérifier que tout fonctionnait bien jusqu'à maintenant. Je vais ajouté d'autres pieux et des effets à ceux-ci afin qu'ils puissent être intéractifs (par exemple, qu'ils sortent du plancher à une certaine fréquence et qu'ils puissent redescendre, qu'ils puissent être tiré, etc).

---
+++ DestinationBangkok/Assets/Scripts/Pièges/Piege.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Piege : MonoBehaviour
+{
+    public GameObject pieu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision infoCollision)
+    {
+        if(infoCollision.gameObject.name=="Manu")
+        {
+            print("Manu a touché le pieu, la partie est finie");
+            SceneManager.LoadScene("MenuPrincipal");
+        }
+    }
+}
