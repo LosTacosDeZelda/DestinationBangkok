30cf987add12369c7f8122e16d0c370ebf15afdf
Frank798034 <Frank798034@users.noreply.github.com>
2020-10-26 12:59:47 -0400

    Déplacement du pieu
    Le pieu se déplace et est détactable par le joueur.

--- DestinationBangkok/Assets/Scripts/Pièges/Piege.cs
+++ DestinationBangkok/Assets/Scripts/Pièges/Piege.cs
@@ -6,36 +6,44 @@ using UnityEngine.SceneManagement;
 public class Piege : MonoBehaviour
 {
     public GameObject pieu;
+    Rigidbody rbPieu;
+    public float distancePieu;
     // Start is called before the first frame update
     void Start()
     {
+        rbPieu = GetComponent<Rigidbody>();
         StartCoroutine(coroutineMonte());
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, Time.deltaTime, 0);
+       // transform.Translate(0, Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider infoCollision)
+    {
+        if (infoCollision.gameObject.tag == "Player")
+        {
+            print("Manu a touché le pieu, la partie est finie");
+            // SceneManager.LoadScene("MenuPrincipal");
+        }
     }
 
     IEnumerator coroutineMonte()
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(2.0f);
+        rbPieu.velocity = new Vector3(0, distancePieu, 0);
+        print("Première coroutine");
         yield return StartCoroutine(coroutineDescend());
 
     }
 
     IEnumerator coroutineDescend()
     {
-        yield return new WaitForSeconds(1.0f);
-    }
-
-    private void OnCollisionEnter(Collision infoCollision)
-    {
-        if(infoCollision.gameObject.name=="Manu")
-        {
-            print("Manu a touché le pieu, la partie est finie");
-            SceneManager.LoadScene("MenuPrincipal");
-        }
+        yield return new WaitForSeconds(2.0f);
+        rbPieu.velocity = new Vector3(0, -distancePieu, 0);
+        print("Deuxième coroutine");
+        yield return StartCoroutine(coroutineMonte());
     }
 }
