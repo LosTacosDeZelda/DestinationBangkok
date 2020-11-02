092aafd2375812705ee2024b6c31ffa6ca7e82d7
LosTacosDeZelda <jeudyraphael@gmail.com>
2020-11-02 13:27:10 -0500

    Détection des collisions des projectiles

---
+++ DestinationBangkok/Assets/Scripts/Pièges/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [HideInInspector]
+    public string typePiegeStr;
+   
+    private void OnTriggerEnter(Collider trigEnter)
+    {
+        print(typePiegeStr);
+        if (trigEnter.gameObject.tag == "Player")
+        {
+            trigEnter.gameObject.GetComponent<PlayerController>().JoueurBlesse(typePiegeStr);
+        }
+       
+    }
+}
