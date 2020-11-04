25289927767c8824017ee2409576c651fc317422
jijimy <jijimy123@gmail.com>
2020-11-04 16:28:03 -0500

    gestion de status fonctionnels

---
+++ DestinationBangkok/Assets/Scripts/Joueur/GestionStatus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestionStatus : MonoBehaviour
+{
+
+
+  public Text texteFeu;
+  public Text textePerfo;
+  public Text textePoison;
+  public Dictionary<string, int> listeCompteStatus;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    listeCompteStatus = new Dictionary<string, int>() {
+            {"Flamme", 0} ,
+            {"Perforation", 0} ,
+            {"Poison", 0} ,
+        };
+  }
+
+  public void MettreAJourTexteFeu()
+  {
+    texteFeu.text = $"{listeCompteStatus["Flamme"]} / 3 Feu";
+    textePerfo.text = $"{listeCompteStatus["Perforation"]} / 3 Perforation";
+    textePoison.text = $"{listeCompteStatus["Poison"]} / 3 Poison";
+  }
+
+}
